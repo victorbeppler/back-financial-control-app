986ff66a6cee964cf9e10928dae3e50f738b9c5f
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-08 21:20:16 -0300

    Build( ent route create user)

--- src/database/view/user.js
+++ src/database/view/user.js
@@ -3,22 +3,34 @@ import crypto from "crypto";
 
 export class User {
   async create(data) {
-    const password = data.password; // a senha que você quer codificar
     const salt = crypto.randomBytes(16).toString("hex"); // uma string aleatória usada como "sal" para tornar o hash mais seguro
     const hash = crypto
-      .pbkdf2Sync(password, salt, 1000, 64, "sha256")
+      .pbkdf2Sync(data.password, salt, 1000, 64, "sha256")
       .toString("hex"); // o hash da senha usando PBKDF2 com SHA-256
     try {
       const response = await prisma.user.create({
         data: {
           name: data.name,
           email: data.email,
-          password: password,
+          password: hash,
+        },
+      });
+      return { success: "Usuário criado com sucesso!" };
+    } catch (e) {
+      console.log(e.meta.target);
+      return { error: "Erro ao criar usuário!", type: e.meta.target };
+    }
+  }
+  async consult(email) {
+    try {
+      const response = await prisma.user.findUnique({
+        where: {
+          email: email,
         },
       });
       return response;
     } catch (e) {
-      console.log(e.meta);
+      console.log(e.meta.targer);
     }
   }
 }
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -5,8 +5,15 @@ const route = express.Router();
 
 route.post("/user/create", async (req, res) => {
   const data = req.body;
-  const response = await new User().create(data);
-  res.json(response);
+  const validateEmail = await new User().consult(data.email);
+  if (validateEmail)
+    return res.status(401).json({ error: "Email já cadastrado!" });
+  else {
+    const response = await new User().create(data);
+    if (response.success)
+      res.status(201).json({ success: "Usuário criado com sucesso!" });
+    else res.status(401).json(response);
+  }
 });
 
 export default route;
