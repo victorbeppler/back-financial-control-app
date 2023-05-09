4947731af776a707f76fb8f6531be7ce1ddd5659
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-09 13:05:08 -0300

    Refactor(update route user create) - Add route create in new controller (CreateUserController)

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,19 +1,8 @@
 import express from "express";
-import { User } from "../database/view/user.js";
+import { CreateUserController } from "../controller/create-user-controller.js";
 
 const route = express.Router();
 
-route.post("/user/create", async (req, res) => {
-  const data = req.body;
-  const validateEmail = await new User().consult(data.email);
-  if (validateEmail)
-    return res.status(401).json({ error: "Email já cadastrado!" });
-  else {
-    const response = await new User().create(data);
-    if (response.success)
-      res.status(201).json({ success: "Usuário criado com sucesso!" });
-    else res.status(401).json(response);
-  }
-});
+route.post("/user/create", await new CreateUserController().handle);
 
 export default route;
