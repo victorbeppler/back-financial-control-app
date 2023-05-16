a1c83d061ae188a3baeb99114a0d79f862378438
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-16 12:32:37 -0300

    Build(import controller create transaction)

--- src/routes/transaction-routes.js
+++ src/routes/transaction-routes.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { CreateTransactionController } from "../controller/create-transaction-controller.js";
 
 const route = express.Router();
+
+route.post(
+  "/transaction/create",
+  await new CreateTransactionController().handle
+);
 
 export default route;
