db7bde8015adc88af26a5dde285dd99d9824f5f7
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-16 12:29:26 -0300

    fix(rename file category)

--- src/routes/category-routes.js
+++ src/routes/category-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { CreateCategoryController } from "../controller/create-ctaegory-controller.js";
+import { CreateCategoryController } from "../controller/create-category-controller.js";
 
 const route = express.Router();
 
