c0948a6557ab24204f506c8cea87a503eda7e6eb
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-18 23:38:16 -0300

    Build(end controller transaction create) - Construct return response create transaction

--- src/controller/create-transaction-controller.js
+++ src/controller/create-transaction-controller.js
@@ -1,5 +1,13 @@
+import { Transaction } from "../database/view/transaction.js";
+
 export class CreateTransactionController {
   async handle(req, res) {
     const data = req.body;
+    const response = await new Transaction().create(data);
+    console.log(response);
+    if (response.success) {
+      return res.status(201).json(response);
+    }
+    return res.status(400).json(response?.error);
   }
 }
