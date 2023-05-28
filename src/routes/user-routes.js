ab6cc36a33473260fcef39030180af6eb49ac84c
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-28 19:01:50 -0300

    Feat(add new route in users route) - route named user/session

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { CreateUserController } from "../controller/create-user-controller.js";
+import { UserSessionController } from "../controller/user-session-controller.js";
 
 const route = express.Router();
 
 route.post("/user/create", await new CreateUserController().handle);
+
+route.post("/user/session", await new UserSessionController().handle);
 
 export default route;
