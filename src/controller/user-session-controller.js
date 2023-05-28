8054ea77d46fb8c05954d34c86984520fffd2a3f
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-28 18:56:39 -0300

    Build(create controller user session) this controller responsible to authenticate user in login

---
+++ src/controller/user-session-controller.js
@@ -0,0 +1,35 @@
+import { User } from "../database/view/user.js";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+
+export class UserSessionController {
+  async handle(req, res) {
+    const secretKey = "a2d3f4g5h6j7k8l9mnht2";
+    // console.log(req);
+    const { email, password } = req.body;
+    console.log(email);
+    console.log(password);
+    const user = await new User().consult(email);
+    console.log(user);
+    if (!user) {
+      console.log("Usuário não encontrado!");
+      return res.status(400).json({ error: "Usuário não encontrado!" });
+    }
+    const hashVerificador = crypto
+      .pbkdf2Sync(password, user.salt, 1000, 64, "sha256")
+      .toString("hex");
+    if (user.password === hashVerificador) {
+      const token = jwt.sign({ id: user.id }, secretKey, {
+        expiresIn: "24h", // expires in 5min
+      });
+      res.cookie("token", token, { httpOnly: true });
+      res.status(200).json({
+        success: "Login realizado com sucesso!",
+        token: token,
+        user: user,
+      });
+    } else {
+      res.status(400).json({ error: "Senha inválida!" });
+    }
+  }
+}
