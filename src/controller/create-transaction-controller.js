8f8d137d0045df4c45e208fe94274d0e900e9c43
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-06 00:27:22 -0300

    Refactor(remove line console.log)

--- src/controller/create-transaction-controller.js
+++ src/controller/create-transaction-controller.js
@@ -4,7 +4,6 @@ export class CreateTransactionController {
   async handle(req, res) {
     const data = req.body;
     const response = await new Transaction().create(data);
-    console.log(response);
     if (response.success) {
       return res.status(201).json(response);
     }
