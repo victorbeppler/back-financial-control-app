a8f55699d374a0b62f329f733cb7a15c10d55c0b
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-06 00:35:57 -0300

    Refactor(modify method create environment) - modify method create environment, add new case connect a intermediate table userEnviroment - add new method consultByUser this method consult environment related user loged

--- src/database/view/environment.js
+++ src/database/view/environment.js
@@ -8,14 +8,40 @@ export class Environment {
         data: {
           name: data.name,
           description: data.description,
-          users: {
-            connect: data.users,
-          },
         },
       });
+      console.log(response.id);
+      if (response) {
+        const associateUser = await prisma.userEnvironment.create({
+          data: {
+            user: {
+              connect: { id: data.user },
+            },
+            environment: {
+              connect: { id: response.id },
+            },
+          },
+        });
+      }
       return { success: "Ambiente criado com sucesso!" };
     } catch (e) {
       return { error: "Erro ao criar ambiente!", type: e?.meta?.target };
+    }
+  }
+  async consultByUser(idUser) {
+    try {
+      const response = await prisma.userEnvironment.findMany({
+        where: {
+          userId: parseInt(idUser),
+        },
+        select: {
+          environment: true,
+        },
+      });
+      return { success: response };
+    } catch (e) {
+      console.log(e);
+      return { error: "Erro ao consultar ambiente!", type: e?.meta?.target };
     }
   }
 }
