671cc94774d8bdae634ec99d313eb005207537be
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-06 00:40:03 -0300

    Build(new route Consult Environment By User) this route responsive to consult environment related a specific user

--- src/routes/environment-routes.js
+++ src/routes/environment-routes.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { CreateEnvironmentController } from "../controller/create-environment-controller.js";
+import { ConsultenvironmentByUserController } from "../controller/consult-environment-by-user-controller.js";
 
 const route = express.Router();
 
-route.post("/environment/create", await new CreateEnvironmentController().handle);
+route.post(
+  "/environment/create",
+  await new CreateEnvironmentController().handle
+);
+
+route.get(
+  "/environment/:idUser",
+  new ConsultenvironmentByUserController().handle
+);
 
 export default route;
