ebcc113163660fdef90536947bbefe6dced90e36
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-06 00:26:34 -0300

    Build(create controller environment consult) this controller have a propose the consult environment associated user. The controller used ID User for the consult environment associated user.

---
+++ src/controller/consult-environment-by-user-controller.js
@@ -0,0 +1,11 @@
+import { Environment } from "../database/view/environment.js";
+
+export class ConsultenvironmentByUserController {
+  async handle(req, res) {
+    const { idUser } = req.params;
+    console.log(idUser);
+    const response = await new Environment().consultByUser(idUser);
+    if (response.success) res.status(200).json(response);
+    else res.status(401).json(response);
+  }
+}
