215bf51fe7a16bdec30fa18ddb11bc25a618cdb2
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-06 00:22:18 -0300

    build(create controller) create new controller category. this controller consult category using receive ID User and ID Environment. information used in view to consult category related

---
+++ src/controller/consult-category-by-user-controller.js
@@ -0,0 +1,16 @@
+import { Category } from "../database/view/category.js";
+
+export class ConsultCategoryByUserController {
+  async handle(request, response) {
+    const { idUser, idEnvironment } = request.params;
+    console.log(idUser, idEnvironment);
+    const category = await new Category().consultByUserEnvironment(
+      idUser,
+      idEnvironment
+    );
+    if (category.error) {
+      return response.status(400).json(category);
+    }
+    return response.status(200).json(category);
+  }
+}
