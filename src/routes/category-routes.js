cbe599d39c3384a45f76bb1fdbfa65f2325ab88d
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-06 00:17:42 -0300

    build(add new route) - add route consult category by user and environment - remove await in route

--- src/routes/category-routes.js
+++ src/routes/category-routes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { CreateCategoryController } from "../controller/create-category-controller.js";
+import { ConsultCategoryByUserController } from "../controller/consult-category-by-user-controller.js";
 
 const route = express.Router();
 
-route.post("/category/create", await new CreateCategoryController().handle);
+route.post("/category/create", new CreateCategoryController().handle);
+
+route.get(
+  "/category/:idEnvironment/:idUser",
+  new ConsultCategoryByUserController().handle
+);
 
 export default route;
