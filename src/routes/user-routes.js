17b0372cdf2264997d2def3e0e20351261d88201
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-30 19:28:43 -0300

    Fix(modify base routes user)

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { CreateUserController } from "../controller/create-user-controller.js";
 import { UserSessionController } from "../controller/user-session-controller.js";
+import { CheckTokenUserController } from "../controller/check-token-user-controller.js";
 
 const route = express.Router();
 
-route.post("/user/create", await new CreateUserController().handle);
+route.post("/user/create", new CreateUserController().handle);
 
-route.post("/user/session", await new UserSessionController().handle);
+route.post("/user/session", new UserSessionController().handle);
+
+route.post("/user/checktoken/:token", new CheckTokenUserController().handle);
 
 export default route;
