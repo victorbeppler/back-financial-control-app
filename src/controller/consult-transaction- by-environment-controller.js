bcbbbe8692a0038e2580f51787886c7539a22fe5
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-30 19:24:24 -0300

    Refactor(change method in controller) Change method consult transaction this method consult transaction by year, month and environment)

--- src/controller/consult-transaction- by-environment-controller.js
+++ src/controller/consult-transaction- by-environment-controller.js
@@ -4,38 +4,44 @@ import { User } from "../database/view/user.js";
 
 export class ConsultTransactionByEnvironmentController {
   async handle(req, res) {
-    const { idEnvironment } = req.params;
-    const transactions = await new Transaction().consultByEnvironment(
-      idEnvironment.replace(/\D/g, "")
-    );
-    let response = [];
-    if (transactions) {
-      for (const transaction of transactions) {
-        console;
-        const user = await new User().consultById(transaction.userId);
-        const category = await new Category().consultById(
-          transaction.categoryId
-        );
-        response.push({
-          id: transaction.id,
-          description: transaction.description,
-          amount: transaction.amount,
-          dueDate: transaction.dueDate,
-          status: transaction.status,
-          environment: transaction.environmentId,
-          user: {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-          },
-          category: {
-            id: category.id,
-            name: category.name,
-            type: category.type,
-          },
-        });
+    try {
+      const { idEnvironment, yearMonth } = req.params;
+      // console.log(idEnvironment, yearMonth);
+      const transactions = await new Transaction().consultByMonthAndEnvironment(
+        idEnvironment?.replace(/\D/g, ""),
+        yearMonth
+      );
+      let response = [];
+      if (transactions) {
+        for (const transaction of transactions) {
+          // console;
+          const user = await new User().consultById(transaction.userId);
+          const category = await new Category().consultById(
+            transaction.categoryId
+          );
+          response.push({
+            id: transaction.id,
+            description: transaction.description,
+            amount: transaction.amount,
+            dueDate: transaction.dueDate,
+            status: transaction.status,
+            environment: transaction.environmentId,
+            user: {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            },
+            category: {
+              id: category.id,
+              name: category.name,
+              type: category.type,
+            },
+          });
+        }
+        return res.status(200).json(response);
       }
-      return res.status(200).json(response);
+    } catch (e) {
+      return res.status(400).json({ error: "Erro ao consultar transações!" });
     }
   }
 }
